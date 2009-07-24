c47bf696e985ca73c362ee3be41fbf9fddb835bd
Jésus Lopes <jtadeulopes@gmail.com>
2009-07-24 00:45:52 -0300

    Alterado nome do metodo.

--- lib/inputs.rb
+++ lib/inputs.rb
@@ -1,15 +1,11 @@
 #
-# Inputs Adiciona novos helpers para criar mascaras em campos de formulário.
-#
-# Baseado no Masked Input Plugin para jQuery criado por Josh Bush
-#
-# Requer: jRails
+# Adiciona helpers para criar text_field com mascaras.
 #
 module Inputs
   
-  #   Retorna um text_field 
+  #   Monta o text_field
   #
-  #   text_field_masked(:user, :phone, :mask => '(99)9999-9999')
+  #   masked_text_field(:user, :phone, :mask => '(99)9999-9999')
   #
   #   # => <input id="user_phone" name="user[phone]" size="30" type="text" />
   #   # => <script type="text/javascript">
@@ -18,7 +14,7 @@ module Inputs
   #         //]]>
   #       </script>
   #
-  def text_field_masked(object,method,mask={})
+  def masked_text_field(object,method,mask={})
     text_field(object,method) +
     javascript_tag("$('##{object}_#{method}').mask('#{mask.values.first}');")
   end
