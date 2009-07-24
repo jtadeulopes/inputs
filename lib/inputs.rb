e74633eeecf4edf44df016767f453ee8788320cf
Jésus Lopes <jtadeulopes@gmail.com>
2009-07-24 10:47:17 -0300

    Created tests and fixes in README

--- lib/inputs.rb
+++ lib/inputs.rb
@@ -1,28 +1,30 @@
 #
-# Adiciona helpers para criar text_field com mascaras.
+# Add helpers to create text_field with masks.
 #
 module Inputs
   
-  #   Monta o text_field
+  #   masked_text_field(:user, :phone, :size => 13, :mask => '(99)9999-9999')
   #
-  #   masked_text_field(:user, :phone, :mask => '(99)9999-9999')
-  #
-  #   # => <input id="user_phone" name="user[phone]" size="30" type="text" />
+  #   # => <input id="user_phone" name="user[phone]" size="13" type="text" />
   #   # => <script type="text/javascript">
   #         //<![CDATA[
   #           $('#user_phone').mask('(99)9999-9999');
   #         //]]>
   #       </script>
   #
-  def masked_text_field(object,method,mask={})
-    text_field(object,method) +
-    javascript_tag("$('##{object}_#{method}').mask('#{mask.values.first}');")
+  def masked_text_field(object, method, options = {})
+    raise "Option mask is required" unless options.has_key?(:mask)
+    mask = options[:mask]
+    options.delete(:mask)
+    text_field(object, method, options) +
+    javascript_tag("$('##{object}_#{method}').mask('#{mask}');")
   end
 
-  # Adiciona arquivos javascripts necessários
+  # Add required files
   #
   # View:
   # <%= javascript_include_inputs %>
+  #
   def javascript_include_inputs
     javascript_include_tag 'maskedinput'
   end
--- test/inputs_test.rb
+++ test/inputs_test.rb
@@ -1,8 +1,38 @@
-require 'test_helper'
+require File.expand_path(File.join(File.dirname(__FILE__), '../../../../test/test_helper'))
 
-class InputsTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+class InputsTest < Test::Unit::TestCase 
+
+  include Inputs
+
+  include ActionView::Helpers::TagHelper
+  include ActionView::Helpers::FormHelper
+  include ActionView::Helpers::JavaScriptHelper
+  include ActionView::Helpers::AssetTagHelper
+
+  def test_masked_text_field_phone
+    assert_dom_equal %(<input name=\"user[phone]\" size=\"30\" id=\"user_phone\" type=\"text\" /><script type=\"text/javascript\">\n//<![CDATA[\n$('#user_phone').mask('(99)9999-9999');\n//]]>\n</script>),
+    masked_text_field(:user, :phone, :mask => '(99)9999-9999')
   end
+
+  def test_masked_text_field_date
+    assert_dom_equal %(<input name=\"user[birth]\" size=\"30\" id=\"user_birth\" type=\"text\" /><script type=\"text/javascript\">\n//<![CDATA[\n$('#user_birth').mask('99/99/9999');\n//]]>\n</script>),
+    masked_text_field(:user, :birth, :mask => '99/99/9999')
+  end
+
+  def test_masked_text_field_date_with_size_10
+    assert_dom_equal %(<input name=\"user[birth]\" size=\"10\" id=\"user_birth\" type=\"text\" /><script type=\"text/javascript\">\n//<![CDATA[\n$('#user_birth').mask('99/99/9999');\n//]]>\n</script>),
+    masked_text_field(:user, :birth, :size => 10, :mask => '99/99/9999')
+  end
+
+  def test_return_error_if_mask_option_nil
+    assert_raise RuntimeError do
+      masked_text_field(:user, :phone)
+    end
+  end
+
+  def test_javascript_include_inputs
+    assert_dom_equal %(<script src=\"/javascripts/maskedinput.js?1248409652\" type=\"text/javascript\"></script>),
+      javascript_include_inputs
+  end
+
 end
