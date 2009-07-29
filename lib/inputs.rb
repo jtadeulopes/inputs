9a3bfc99a51a79ca127f7a77c379ed4e521682dc
Jésus Lopes <jtadeulopes@gmail.com>
2009-07-29 13:05:18 -0300

    Fixes in the README and documentation methods.

--- lib/inputs.rb
+++ lib/inputs.rb
@@ -1,10 +1,13 @@
-#
-# Add helpers to create text_field with masks.
-#
 module Inputs
   
-  #   masked_text_field(:user, :phone, :size => 13, :mask => '(99)9999-9999')
+  # Create text_field with mask.
   #
+  # Example:
+  #   
+  #   # View:
+  #   <%= masked_text_field(:user, :phone, :size => 13, :mask => '(99)9999-9999') %>
+  #
+  #   # Output:
   #   # => <input id="user_phone" name="user[phone]" size="13" type="text" />
   #   # => <script type="text/javascript">
   #         //<![CDATA[
@@ -19,14 +22,21 @@ module Inputs
       javascript_tag("$('##{object}_#{method}').mask('#{mask}');")
   end
 
-  #   price_us_text_field(:product, :price, :size => 10)
+  # Create text_field with masks for price
   #
+  # Example:
+  #
+  #   # View:
+  #   <%= price_us_text_field(:product, :price, :size => 10) %>
+  #
+  #   # Output:
   #   # => <input id="product_price" name="product[price]" size="10" type="text" />
   #   # => <script type="text/javascript">
   #         //<![CDATA[
   #           $('#product_price').priceFormat();
   #         //]]>
   #       </script>
+  #
   def price_us_text_field(object, method, options = {})
     text_field(object, method, options) + 
       javascript_tag("$('##{object}_#{method}').priceFormat();")
@@ -34,8 +44,9 @@ module Inputs
 
   # Add required files
   #
-  # View:
-  # <%= javascript_include_inputs %>
+  #   # View:
+  #   <%= javascript_include_tag :defaults %>
+  #   <%= javascript_include_inputs %>
   #
   def javascript_include_inputs
     javascript_include_tag 'maskedinput', 'price'
